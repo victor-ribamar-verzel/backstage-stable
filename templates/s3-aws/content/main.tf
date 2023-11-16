82de7a2006a6e8a5687013cb14296ed598a54e18
victor.ribamar.verzel <victor.ribamar@outlook.com>
2023-11-16 16:20:21 -0300

    [skip ci] adiciona templates

---
+++ templates/s3-aws/content/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "~> 4.0"
+        }
+      }
+    backend "s3" {
+      bucket = "verzel-tfstate"
+      key    = "backstage/infra-as-a-code/tfstate"
+      region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "S3Bucket" {
+  bucket = var.bucket_name
+}
---
+++ templates/s3-aws/content/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+
+variable "bucket_name" {
+  description = "nome do bucket"
+  type        = string
+  default     = "default"
+}
